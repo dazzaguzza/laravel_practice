7edcd487f4c7b4b54ab99742803a26b0f7343e78
jinho <tazzaenciel@gmail.com>
2023-02-21 18:29:53 +0900

    파이프라인 + secret manager 테스트15

--- laravel/scripts/after_install.bash
+++ laravel/scripts/after_install.bash
@@ -1,11 +1,10 @@
 #!/bin/bash 
 cd /var/www/html
-aws secretsmanager get-secret-value --region ap-northeast-2 --secret-id env --query SecretString --output text | sudo tee secret.json
-jq -r '{"KEYS": "VALUES"} + to_entries[] | "\(.key)=\(.value)"' secret.json | sudo tee laravel_practice/laravel/.env
-# sudo rm -f secret.json
-# cd laravel_practice/docker
-# sudo docker-compose up --build -d
+aws secretsmanager get-secret-value --region ap-northeast-2 --secret-id env --query SecretString --output text | sudo tee env.json
+jq -r '{"KEYS": "VALUES"} + to_entries[] | "\(.key)=\(.value)"' env.json | sudo tee laravel_practice/laravel/.env
+sudo rm -f env.json
+cd laravel_practice/docker
+sudo docker-compose up --build -d
 
  
 
- 
