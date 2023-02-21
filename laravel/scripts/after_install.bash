8a0b08006b944b841fa7db77408c95cfffad2ab3
jinho <tazzaenciel@gmail.com>
2023-02-21 17:54:58 +0900

    파이프라인 + secret manager 테스트12

--- laravel/scripts/after_install.bash
+++ laravel/scripts/after_install.bash
@@ -3,6 +3,6 @@ cd /var/www/html
 aws secretsmanager get-secret-value --region ap-northeast-2 --secret-id env --query SecretString --output text | sudo tee secret.json
 jq -r '{"KEYS": "VALUES"} + to_entries[] | "\(.key)=\(.value)"' secret.json | sudo tee laravel_practice/laravel/.env
 # sudo rm -f secret.json
-cd laravel_practice/docker
-sudo docker-compose up --build -d
+# cd laravel_practice/docker
+# sudo docker-compose up --build -d
 
--- laravel/scripts/before_install.bash
+++ laravel/scripts/before_install.bash
@@ -1,8 +1,7 @@
 #!/bin/bash
 cd /var/www/html/laravel_practice/docker
 sudo docker system prune -f
-sudo docker container stop $(docker container ls -aq)
-sudo docker container rm $(docker container ls -aq)
-sudo docker rmi $(docker images -aq)
+sudo docker rm $(sudo docker ps -q)
+sudo docker rmi $(sudo docker images -q)
 sudo docker volume prune -f
 rm -rf /var/www/html/laravel_practice   
