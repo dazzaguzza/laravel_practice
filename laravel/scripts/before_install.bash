3f6d5f74f3dcb6b900a687cd100316226a29dd7d
jinho <tazzaenciel@gmail.com>
2023-02-22 15:14:15 +0900

    css 테스트

--- laravel/scripts/before_install.bash
+++ laravel/scripts/before_install.bash
@@ -4,4 +4,4 @@ sudo docker rm -f $(sudo docker ps -q)
 sudo docker rmi -f $(sudo docker images -q)
 sudo docker system prune -f
 sudo docker volume prune -f
-sudo rm -rf /var/www/html/laravel_practice   
+sudo rm -rf /var/www/html/laravel_practice  
